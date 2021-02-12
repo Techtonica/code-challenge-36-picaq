59c36436444053a8d1a7583e4e8207dc6b6b36c9
Mandy <34908590+picaq@users.noreply.github.com>
2021-02-12 09:24:59 -0800

    complete challenge 36.js make_tags with template literal

--- challenges/36.js
+++ challenges/36.js
@@ -1,3 +1,4 @@
+
 // The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
 //
 // make_tags('i', 'Yay') → '<i>Yay</i>'
@@ -8,8 +9,11 @@
 
 function make_tags(input_tag, input_string){
   // Add functionality here!
-  return
+  return `<${input_tag}>${input_string}</${input_tag}>`;
 }
 
+// console.log(make_tags('i', 'Yay')); //→ '<i>Yay</i>')
+// console.log(make_tags('i', 'Hello')); //→ '<i>Hello</i>')
+// console.log(make_tags('cite', 'Yay')); //→ '<cite>Yay</cite>')
 
 module.exports = make_tags;
